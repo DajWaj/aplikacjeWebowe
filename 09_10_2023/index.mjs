a8691fb4c8644978e7ecfadf721b82ae2e585923
DajWaj <dajwaj.glowny@gmail.com>
2023-10-15 22:36:15 +0200

    aha

---
+++ 09_10_2023/index.mjs
@@ -0,0 +1,15 @@
+import { Readable, pipeline } from "stream";
+import fs from "fs";
+
+async function* generate() {
+  for (let i = 0; i < 20; i++) {
+    yield (Math.floor(Math.random() * (2137 - -420)) + -420).toString() + "\n";
+  }
+}
+
+const readable = Readable.from(generate());
+const writable = fs.createWriteStream(`random-${Date.now()}.txt`);
+
+readable.on("data", (chunk) => {
+  writable.write(chunk);
+});
