943bbe1a002693e1ee837b174096c9f56ef3f282
BruhGuy123453 <janiemoge.xdd3@gmail.com>
2023-10-15 22:10:37 +0200

    lala

--- 02_10_2023/index.js
+++ 02_10_2023/index.js
@@ -1,28 +1,84 @@
 const http = require('http');
-const fs = require('fs');
-const index = fs.readFileSync('index.html');
-const dziekujemy = fs.readFileSync('dziekujemy.html');
-const api = fs.readFileSync('api.html');
+const fs = require('fs/promises');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
     const url = req.url;
+    const method = req.method;
     if(url==='/'){
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        res.end(index);
+        const html = await fs.readFile('index.html');
+        res.write(html);
+        res.end();
     }
-    if(url==='/dziekujemy'){
+    else if(url==='/styleIndex.css'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/css');
+        const css = await fs.readFile('styleIndex.css');
+        res.write(css);
+        res.end();
+    }
+    else if(url==='/dziekujemy'){
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
-        res.end(dziekujemy);
+        const html = await fs.readFile('dziekujemy.html');
+        res.write(html);
+        res.end();
     }
-    if(url==='/api'){
+    else if(url==='/styleDziekujemy.css'){
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end(api);
+        res.setHeader('Content-Type', 'text/css');
+        const css = await fs.readFile('styleDziekujemy.css');
+        res.write(css);
+        res.end();
+    }
+    else if(url==='/api'){
+        function Worker(name, surname, age) {
+            this.name = name
+            this.surname = surname
+            this.age = age
+        }
+        const w1 = new Worker('Juan', 'Pablo', 67)
+        const w2 = new Worker('Karol', 'Wojtyla', 52)
+        const w3 = new Worker('Marcin', 'Prokop', 56)
+        const workers = [w1,w2,w3]
+
+        res.statusCode = 200
+        res.setHeader('content-type', 'application/json')
+        res.write(JSON.stringify(workers))
+        res.end()
+    }
+    else if(url==='/styleApi.css'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/css');
+        const css = await fs.readFile('styleDziekujemy.css');
+        res.write(css);
+        res.end();
+    }
+    else if (url==='/kontakt' && method =='POST') {
+        const body = []
+        req.on('data', (chunk) => {
+            console.log(chunk.toString())
+            body.push(chunk)
+        })
+
+        req.on('end', async () => {
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=')[1]
+            await fs.writeFile(`./contact/message-${Date.now().toString()}.txt`, message)
+            res.statusCode = 302
+            res.setHeader('Location', '/dziekujemy')
+            return res.end()
+        })
+    }
+    else {
+        res.statusCode = 404
+        res.setHeader('content-type', 'application/json')
+        res.write('{"error": "404 this page does not exist"}')
+        res.end()
     }
 });
  
