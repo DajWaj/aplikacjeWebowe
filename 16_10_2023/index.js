640ef8256c91b5cd5e031ee9c06ce511d456b827
DajWaj <dajwaj.glowny@gmail.com>
2023-10-30 01:19:14 +0100

    kuf

--- 16_10_2023/index.js
+++ 16_10_2023/index.js
@@ -1,56 +1,33 @@
-const http = require('http');
-const fs = require('fs/promises');
-const express = require("express");
+const express = require('express');
+const path = require('path');
 
-const hostname = '127.0.0.1';
+const app = express();
+const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer(async (req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if(url==='/'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        const html = await fs.readFile('index.html');
-        res.write(html);
-        res.end();
-    }
-    else if(url==='/styleIndex.css'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/css');
-        const css = await fs.readFile('styleIndex.css');
-        res.write(css);
-        res.end();
-    }
-    else if (url==='/kontakt') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        const html = await fs.readFile('kontakt.html');
-        res.write(html);
-        res.end();
-    }
-    else if(url==='/styleKontakt.css'){
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/css');
-        const css = await fs.readFile('styleKontakt.css');
-        res.write(css);
-        res.end();
-    }
-    else if (url==='/uga.js') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/javascript');
-        const js = await fs.readFile('./public/js/uga.js');
-        res.write(js);
-        res.end();
-    }
-    else {
-        res.statusCode = 404
-        res.setHeader('content-type', 'application/json')
-        res.write('{"error": "404 this page does not exist"}')
-        res.end()
-    }
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
- 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+
+app.route('/kontakt')
+    .get(async (req, res) => {
+        res.sendFile(path.join(__dirname, 'kontakt.html'));
+    })
+
+    .post((req, res) => {
+        console.log('Form send')
+    })
+
+app.get('/styleIndex.css', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/css/styleIndex.css'));
 });
+
+app.get('/styleKontakt.css', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/css/styleKontakt.css'));
+});
+
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`)
+})
